52e5ef6e5ac4b74c8282196ac8bf5a5136c592d4
Bemar <Bemar@DESKTOP-5JOPI67>
2022-05-28 17:03:03 +0300

    .

---
+++ Function/Credits.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    public class CreditsClass
+    {
+
+        public void installment(int kp, int tutar) 
+        //Fonksiyonda kullanacağımız değerleri her zaman parantez içinde tipiyle beraber (int, double vs.) tanımlıyoruz.
+        {
+            double toplamtutar;
+            if (kp == 1) { toplamtutar = tutar + (tutar * 0.2); Console.Write("Ödemeniz Gereken Tutar=" + toplamtutar + " TL"); }
+            if (kp == 2) { toplamtutar = tutar + (tutar * 0.3); Console.Write("Ödemeniz Gereken Tutar=" + toplamtutar + " TL"); }
+            if (kp == 3) { toplamtutar = tutar + (tutar * 0.4); Console.Write("Ödemeniz Gereken Tutar=" + toplamtutar + " TL"); }
+            if (kp == 4) { toplamtutar = tutar + (tutar * 0.5); Console.Write("Ödemeniz Gereken Tutar=" + toplamtutar + " TL"); }
+            if (kp == 5) { toplamtutar = tutar + (tutar * 0.6); Console.Write("Ödemeniz Gereken Tutar=" + toplamtutar + " TL"); }
+            if (kp>5 || kp<1) { Console.Write("Mevcut Paket Bulunmamaktadır"); }  
+            
+        
+        
+        
+        }
+
+
+    }
+}
--- Function/Program.cs
+++ Function/Program.cs
@@ -1,4 +1,4 @@
-﻿// static void WriteName() { Console.WriteLine("Berkay Balon"); }
+﻿//  static void WriteName() { Console.WriteLine("Berkay Balon"); }
 
 
 //Fonksiyon tanımlarken "public veya private ile tanımlıyoruz. Direk "static void" olarak yazdığımızda public oluyor ve her yerden erişebiliyoruz.
@@ -7,7 +7,7 @@
 //Public  -->  Metod, programın tamamında kullanılabilir.
 //Static  -->  Private ve Public ifadesinden sonra eklenirse metod doğrudan kullanılabilir, eklenmezse metodun bulunduğu sınıftan nesne türetilerek kullanılmak zorundadır.
 
-// WriteName();
+//  WriteName();
 
 //Fonksiyonun tanımı (ismi). Fonksiyonu kullanırken bu isimle çağırıyoruz.
 
@@ -15,17 +15,49 @@
 //** İnteger döndüren fonksiyon
 
 
-static int top(int s1, int s2) { return s1 + s2; }
+//  static int top(int s1, int s2) { return s1 + s2; }
 //Fonksiyonu en başta tanımlarsak daha rahat ederiz bence.
 
-Console.Write("1. Sayıyı Giriniz=");
-int s1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("2. Sayıyı Giriniz=");
-int s2 = Convert.ToInt32(Console.ReadLine());
-int s3 = top(s1, s2);
-// top(s1, s2) yazdığımız yerde fonksiyonu çağırmış oluyoruz.
+//  Console.Write("1. Sayıyı Giriniz=");
+//  int s1 = Convert.ToInt32(Console.ReadLine());
+//  Console.Write("2. Sayıyı Giriniz=");
+//   int s2 = Convert.ToInt32(Console.ReadLine());
+// int s3 = top(s1, s2);
+//top(s1, s2) yazdığımız yerde fonksiyonu çağırmış oluyoruz.
 
-Console.Write("İşlem Sonucu=" + s3);
+//  Console.Write("İşlem Sonucu=" + s3);
+
+
+//** Bankadan çekilen krediyi seçilen mevduadına göre hesaplama. ( Class Açarak Ordan Çekme)
+
+using Functions;
+
+
+
+
+
+Console.Write("Çekmek İstediğiniz Kredi Tutarını Giriniz=");
+int tutar = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("1. Paket= %2 Faiz");
+Console.WriteLine("2. Paket= %3 Faiz");
+Console.WriteLine("3. Paket= %4 Faiz");
+Console.WriteLine("4. Paket= %5 Faiz");
+Console.WriteLine("5. Paket= %6 Faiz");
+Console.Write("Kredi Paketini Giriniz=");
+
+int kp = Convert.ToInt32(Console.ReadLine());
+
+CreditsClass credits = new CreditsClass();
+//sınıfı burda çağırdık. Sınıfı çağırmadan sınıfın içindeki fonksiyonu kullanamayız.
+
+credits.installment(kp,tutar);
+//Fonksiyonu çağırdık ve kullandık. Fonksiyonu kullanırken, fonksiyonda kullandığımız tüm değişkenleri parantez içinde tanımlıyoruz.
+
+
+
+
+
 
 
 
